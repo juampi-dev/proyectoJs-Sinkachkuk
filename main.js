d18bd9d4d76e36817c04c18e83bd949360286f55
juan-ps <juanps.rt@gmail.com>
2022-07-20 23:09:10 -0300

    pre-entrega

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let cantidades = parseInt(prompt("Ingrese las cantidades del producto:"))
 let producto = [nombre, precio, cantidades];
     if (cantidades <= 0){
         alert("Las cantidades ingresadas son invalidas. Por favor, vuelva a intentarlo mas tarde.");   
-    }else if (nombre.length = 0){
+    }else if (nombre.length == 0){
         alert("El nombre ingresado es invalido. Por favor, vuelva a intentarlo mas tarde.");
     }else{
         alert(`El producto ingresado es ${nombre}, cuesta $${precio} (precio unitario sin IVA), y hay ${cantidades} productos disponibles en stock.`);
@@ -27,3 +27,19 @@ alert(`El precio unitario final del producto con IVA es $${precioConIva}.`);
 //Imprimimos el valor total de los productos ingresados
 const patrimonioNeto = (precioConIva * cantidades);
 alert(`El patrimonio total de ${nombre} ingresado es $${patrimonioNeto}.`)
+
+//Ingresamos otros productos en una array para calcular el IVA con un forEach
+
+let stockProductos = [
+    {nombre: "mesas", precio: 500, cantidades: 30},
+    {nombre: "sillas", precio: 250, cantidades: 52},
+    {nombre: "almohadones", precio: 85, cantidades: 18},
+    {nombre: "cajoneras", precio: 1100, cantidades: 9},
+]
+
+stockProductos.forEach(productoEnlistado => {
+    let precioConIvaEnlistado = (productoEnlistado.precio + iva(productoEnlistado.precio));
+    alert(`Tenemos los siguientes productos ya guardados en stock: ${productoEnlistado.nombre}, y su precio sin IVA es ${productoEnlistado.precio}, y contamos con ${productoEnlistado.cantidades} unidades.`);
+    alert(`El precio unitario final de ${productoEnlistado.nombre} es ${precioConIvaEnlistado}`);
+
+})
