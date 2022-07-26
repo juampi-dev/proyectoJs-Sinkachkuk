313e4bf0fb832b31ff518d0a2269aa5d5cf47253
juan-ps <juanps.rt@gmail.com>
2022-07-25 23:26:55 -0300

    DOM

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//Calculamos el IVA con la funcion
+/* const iva = x => x*0.21;
+let precioConIva = (iva(precio) + precio);
+
+//Imprimimos el precio para el consumidor final
+alert(`El precio unitario final del producto con IVA es $${precioConIva}.`);
+
+//Imprimimos el valor total de los productos ingresados
+const patrimonioNeto = (precioConIva * cantidades);
+alert(`El patrimonio total de ${nombre} ingresado es $${patrimonioNeto}.`) */
+
+// --------------------------------------------------------------------------------------------- //
+
+class ProductoNuevo {
+    constructor(nombre, precio, cantidades) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidades = cantidades;
+    }
+}
+
+let listaProductos = [];
+
+const guardaProd = () => {
+    let nombre = document.getElementById("nombre").value;
+    let precio = document.getElementById("precio").value;
+    let cantidades = document.getElementById("cantidades").value;
+    let nuevoProd = new Producto(nombre, precio, cantidades);
+    listaProductos.push(nuevoProd);
+    return nuevoProd;
+}
